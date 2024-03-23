b2f5d3be0ad1df053040807d115c7b4f974cf871
mohithsaran <mohithsaran.kanamarlapudi@gmail.com>
2024-03-23 16:16:59 +0530

    Buttons and display, styling included

---
+++ components/Display.jsx
@@ -0,0 +1,10 @@
+import styles from "./Display.module.css"
+const Display = () => {
+    return (
+        <>
+        <input type="number" className={styles.display} ></input>
+        </>
+      );
+}
+ 
+export default Display;
