c9f203e3591e2165e59fb58bb9849b1a385ea237
mohithsaran <mohithsaran.kanamarlapudi@gmail.com>
2024-03-23 22:22:35 +0530

    Calculator App done

--- components/Display.jsx
+++ components/Display.jsx
@@ -1,8 +1,8 @@
 import styles from "./Display.module.css"
-const Display = () => {
+const Display = ({displayval}) => {
     return (
         <>
-        <input type="number" className={styles.display} ></input>
+        <input type="text" className={styles.display} value={displayval} readOnly></input>
         </>
       );
 }
--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,32 @@
 import styles from "./App.module.css"
 import Display from "../components/Display"
 import ButtonContainer from "../components/ButtonContainer"
+import { useState } from "react"
 
 function App() {
- 
-
+  let [display, setDisplay] = useState("")
+  const onButtonClick = (buttonText) => {
+    if(buttonText==="C"){
+      setDisplay("")
+    }
+    else if(buttonText==="="){
+      const result=eval(display);
+      setDisplay(result)
+  }
+  else{
+    const newDisplay=display+buttonText;
+    setDisplay(newDisplay)
+  }
+  
+}
   return (
     <>
+    <center>
       <div className={styles.calculator}> 
-        <Display/>
-        <ButtonContainer/>
+        <Display displayval={display}></Display>
+        <ButtonContainer onButtonClick={onButtonClick}></ButtonContainer>
       </div>
+      </center>
     </>
   )
 }
